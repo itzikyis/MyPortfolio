593ac74e49120f14095f810c5f2cfa4860b0ee17
itzikyis <itzik.yis@gmail.com>
2021-08-23 09:17:29 +0300

    Fix pdf load

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import itCV from '../itzikCV.pdf'
 import "../css/cv.css";
 import NavBar from './Navbar';
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Cv = (props) => {
     const [numPages, setNumPages] = useState(null);
@@ -21,6 +24,7 @@ const Cv = (props) => {
                         file={itCV}
                         options={{ workerSrc: "../pdf.worker.js" }}
                         onLoadSuccess={onDocumentLoadSuccess}
+                        onLoadError={console.error}
                     >
                         {Array.from(new Array(numPages), (el, index) => (
                             <Page key={`page_${index + 1}`} pageNumber={index + 1} />
