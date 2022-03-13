c3140e61a0675e24dadabaca11bf1df51464034b
itzikyis <itzik.yis@gmail.com>
2022-03-13 10:29:06 +0200

    Updates

---
+++ precache-manifest.2dcebbeced662f6b56a7002e89d32270.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573073aa7d895648b50c07ee8571e9d2",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "4d68c9b1fde20980e053",
+    "url": "/MyPortfolio/static/css/2.59815e44.chunk.css"
+  },
+  {
+    "revision": "7f5055195f61c78ad416",
+    "url": "/MyPortfolio/static/css/main.e71faf04.chunk.css"
+  },
+  {
+    "revision": "4d68c9b1fde20980e053",
+    "url": "/MyPortfolio/static/js/2.eaddf9eb.chunk.js"
+  },
+  {
+    "revision": "67a04d7398cdddb04f42cabca98ce660",
+    "url": "/MyPortfolio/static/js/2.eaddf9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5055195f61c78ad416",
+    "url": "/MyPortfolio/static/js/main.ab9be50f.chunk.js"
+  },
+  {
+    "revision": "0d8bee11d1469486e213",
+    "url": "/MyPortfolio/static/js/runtime-main.7e73d9a0.js"
+  },
+  {
+    "revision": "ba2835f4546fb335191b44a13d5ee57f",
+    "url": "/MyPortfolio/static/media/MyAvatar.ba2835f4.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/MyPortfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "7fbd06c78522bd4de026e3633b94dcb6",
+    "url": "/MyPortfolio/static/media/itzikCV.7fbd06c7.pdf"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/MyPortfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "5282874d043f78cda213516171f9973a",
+    "url": "/MyPortfolio/static/media/netflix-logo.5282874d.jpg"
+  },
+  {
+    "revision": "a74f0d8ff7f871ba14dd5dc4adc955b5",
+    "url": "/MyPortfolio/static/media/react-redux.a74f0d8f.jpg"
+  },
+  {
+    "revision": "5dd72b61e716eae08bc0938e7b9d38f1",
+    "url": "/MyPortfolio/static/media/react.5dd72b61.jpg"
+  }
+]);
